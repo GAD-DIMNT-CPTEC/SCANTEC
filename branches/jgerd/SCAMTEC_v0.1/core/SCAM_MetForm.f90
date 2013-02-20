4eb69b848bba23c1d25702fe187b57fd1218a24d
joao.gerd <joao.gerd@f4d869c9-3b79-40b0-af35-19216b5cd462>
2013-02-20 17:37:59 +0000

    Adicionada rotina para conversao de variaveis

---
+++ branches/jgerd/SCAMTEC_v0.1/core/SCAM_MetForm.f90
@@ -0,0 +1,557 @@
+!-----------------------------------------------------------------------------!
+!           Group on Data Assimilation Development - GDAD/CPTEC/INPE          !
+!-----------------------------------------------------------------------------!
+!BOP
+!
+! !MODULE: SCAM_MetForm.f90
+!
+! !DESCRIPTON: Este modulo tem por proposito apresentar rotinas e funcoes para 
+!              a conversão de variaveis meteorologicas.
+!                 
+!\\
+!\\
+! !INTERFACE:
+!
+
+Module SCAM_MetForm
+
+  IMPLICIT NONE
+  PRIVATE
+  !
+  ! !PARAMETERS:
+  !
+
+  real, parameter  :: p0 = 1000.00 ! pressão de referencia
+  real, parameter  :: Rd =  287.04 ! Constante dos Gases para o ar seco [ Joutes/K/kg]
+  real, parameter  :: Rv =  461.50 ! Constante dos Gases para o Vapor d'água [ Joutes/K/kg]
+  real, parameter  :: cp = 1003.50 ! Calor especifico para o ar seco [Joules/K/kg]
+  real, parameter  :: L  = 2257e03 ! Calor latente de vaporização [Joules/kg]
+
+  integer, parameter :: stdout = 6
+  character(len=1024),parameter :: myname='SCAM_MetForm'
+
+  !
+  ! !PUBLIC MEMBER FUNCTIONS:
+  !
+  
+  public :: es
+  public :: ee
+  public :: w
+  public :: q
+  public :: rh
+  public :: td
+  public :: tv
+
+  interface q
+     module procedure q1,& ! utiliza somente razao de mistura q(w)
+                      q2   ! utiliza td e pressao q(p,td)
+  end interface
+
+  interface w
+     module procedure w1,& ! utiliza pressão de vapor e pressao atmosferica w(p,e)
+                      w2   ! utiliza umidade especifica w(q)
+  end interface
+
+  !
+  ! !REVISION HISTORY:
+  !  19 FebT 2013 - J. G. de Mattos - Initial Version
+  !
+  ! !SEE ALSO:
+  !   
+  ! !BUGS:
+  !
+  !  Nenhum conhecido
+  !
+  !EOP
+  !-----------------------------------------------------------------------------!
+
+Contains
+
+  !
+  !-----------------------------------------------------------------------------!
+  !BOP
+  !
+  ! !IROUTINE:  es
+  !
+  ! !DESCRIPTION: Esta funcao calcula a pressão de vapor de saturacao [Pa]
+  !
+  !\\
+  !\\
+  ! !INTERFACE:
+
+  function es(temp)
+     implicit none
+    !
+    ! !INPUT PARAMETERS:
+    !
+
+    real, intent(in) :: temp ! Temperatura do ar [Graus Celsius]
+
+    !
+    ! !OUTPUT PARAMETERS:
+    !
+
+    real :: es ! pressão de vapor saturado [Pa]
+
+    !
+    !
+    ! !REVISION HISTORY: 
+    !  19 Feb 2013 - J. G. de Mattos - Initial Version
+    !
+    ! !SEE ALSO: 
+    !     Bolton, D., The computation of equivalent potential temperature, 
+    !                  Monthly Weather Review, 108, 1046-1053, 1980.. 
+    !
+    !EOP
+    !-----------------------------------------------------------------------------!
+    !BOC
+    !
+
+    character(len=1024),parameter :: myname_=trim(myname)//'::es'
+
+#ifdef DEBUG
+    WRITE(stdout,'(     2A)')'Hello from ', trim(myname_)
+#endif
+    
+
+    es = 611.2*exp((17.67 * temp)/(temp+243.5))
+
+  end function
+  !EOC
+  !
+  !-----------------------------------------------------------------------------!
+  !BOP
+  !
+  ! !IROUTINE:  ee
+  !
+  ! !DESCRIPTION: Esta função calcula a pressão de vapor [Pa]
+  !
+  !\\
+  !\\
+  ! !INTERFACE:
+  function ee(es, rh)
+     implicit none
+
+     !
+     ! !INPUT PARAMETERS:
+     !
+
+     real, intent(in) :: es ! Pressão de vapor saturado [Pa]
+     real, intent(in) :: rh ! Umidade relativa [%]
+
+     !
+     ! !OUTPUT PARAMETERS:
+     !
+
+     real :: ee ! pressao de vapor [Pa]
+
+     !
+     !
+     ! !REVISION HISTORY: 
+     !  19 Feb 2013 - J. G. de Mattos - Initial Version
+     !
+     ! !SEE ALSO: 
+     !     Huschke, Glossary of Meteorology, 1959, p. 477
+     !      
+     !     
+     !
+     !EOP
+     !-----------------------------------------------------------------------------!
+     !BOC
+     !
+    character(len=1024),parameter :: myname_=trim(myname)//'::ee'
+
+#ifdef DEBUG
+    WRITE(stdout,'(     2A)')'Hello from ', trim(myname_)
+#endif
+
+     ee = ( rh / 100.0 ) * es
+
+  endfunction
+  !EOC
+  !-----------------------------------------------------------------------------!
+  !BOP
+  !
+  ! !IROUTINE:  w
+  !
+  ! !DESCRIPTION: Esta funcao calcula a razao de mistura [-]
+  !               
+  !
+  !\\
+  !\\
+  ! !INTERFACE:
+
+  function w1(pres, ee)
+     implicit none
+
+     !
+     ! !INPUT PARAMETERS:
+     !
+
+     real, intent(in) :: pres  ! pressão atmosferica [Pa]
+     real, intent(in) :: ee    ! pressao de vapor [Pa]
+
+     !
+     ! !OUTPUT PARAMETERS:
+     !
+
+     real :: w1 ! razão de mistura [-]
+
+     !
+     !
+     ! !REVISION HISTORY: 
+     !  19 Feb 2013 - J. G. de Mattos - Initial Version
+     !
+     ! !SEE ALSO: 
+     !     Wallace & Hobbs, Atmospheric Science, 1977, Academic Press 
+     !
+     !     
+     !
+     !EOP
+     !-----------------------------------------------------------------------------!
+     !BOC
+     !
+
+     real :: eps
+
+    character(len=1024),parameter :: myname_=trim(myname)//'::w1'
+
+#ifdef DEBUG
+    WRITE(stdout,'(     2A)')'Hello from ', trim(myname_)
+#endif
+     
+
+     eps = Rd / Rv
+
+     w1 = eps * ( ee / (pres - ee) );  
+
+  endfunction
+  !EOC
+  !-----------------------------------------------------------------------------!
+  !BOP
+  !
+  ! !IROUTINE:  w2
+  !
+  ! !DESCRIPTION: Esta rotina calcula a razao de mistura [kg/kg] a partir da umidade
+  !               especifica [kg/kg].
+  !
+  !\\
+  !\\
+  ! !INTERFACE:
+
+  FUNCTION w2(q)
+
+    Implicit None
+    !
+    ! !INPUT PARAMETERS:
+    !
+    real, intent(in) :: q ! Umidade especifica [kg/kg]
+    !
+    ! !OUTPUT PARAMETERS:
+    !
+    
+    real :: w2 ! Razão de Mistura [kg/kg]
+
+    !
+    !
+    ! !REVISION HISTORY: 
+    !  19 Feb 2013 - J. G. de Mattos - Initial Version
+    !
+    !
+    !EOP
+    !-----------------------------------------------------------------------------!
+    !BOC
+    !
+
+    character(len=1024),parameter :: myname_=trim(myname)//'::w2'
+
+#ifdef DEBUG
+    WRITE(stdout,'(     2A)')'Hello from ', trim(myname_)
+#endif
+
+    w2 = (q / ( 1000.0 - q ) )
+
+  end function
+  !EOC
+  !-----------------------------------------------------------------------------!
+  !BOP
+  !
+  ! !IROUTINE:  q1
+  !
+  ! !DESCRIPTION: Esta funcao calcula a umidade especifica em kg/kg 
+  !               
+  !
+  !\\
+  !\\
+  ! !INTERFACE:
+
+  function q1(w)
+     implicit none
+
+     !
+     ! !INPUT PARAMETERS:
+     !
+
+     real, intent(in) :: w ! razao de mistura em kg/kg
+
+     !
+     ! !OUTPUT PARAMETERS:
+     ! 
+
+     real :: q1 ! Umidade especifica em kg/kg
+
+     !
+     !
+     ! !REVISION HISTORY: 
+     !  19 Feb 2013 - J. G. de Mattos - Initial Version
+     !
+     ! !SEE ALSO: 
+     !     Huschke, Glossary of Meteorology, 1959, p. 530
+     !      
+     !     
+     !
+     !EOP
+     !-----------------------------------------------------------------------------!
+     !BOC
+     !
+    character(len=1024),parameter :: myname_=trim(myname)//'::q1'
+
+#ifdef DEBUG
+    WRITE(stdout,'(     2A)')'Hello from ', trim(myname_)
+#endif
+
+     q1 = w / (1.0 + w)
+     q1 = q1 * 1000.0
+
+  endfunction
+  !EOC
+  !-----------------------------------------------------------------------------!
+  !BOP
+  !
+  ! !IROUTINE:  q2
+  !
+  ! !DESCRIPTION: Esta funcao calcula a umidade especifica em g/kg 
+  !               
+  !
+  !\\
+  !\\
+  ! !INTERFACE:
+
+  function q2(pres,td)
+     implicit none
+
+     !
+     ! !INPUT PARAMETERS:
+     !
+
+     real, intent(in) :: pres ! Pressao Atmosférica [Pa]
+     real, intent(in) :: td   ! Tempeatura do Ponto de Orvalho [C]
+
+     !
+     ! !OUTPUT PARAMETERS:
+     ! 
+
+     real :: q2 ! Umidade especifica em g/kg
+
+     !
+     !
+     ! !REVISION HISTORY: 
+     !  19 Feb 2013 - J. G. de Mattos - Initial Version
+     !
+     ! !SEE ALSO: 
+     !     Bolton, D., The computation of equivalent potential temperature, 
+     !                  Monthly Weather Review, 108, 1046-1053, 1980. 
+     !      
+     !     
+     !
+     !EOP
+     !-----------------------------------------------------------------------------!
+     !BOC
+     !
+     
+     real :: e
+
+    character(len=1024),parameter :: myname_=trim(myname)//'::q2'
+
+#ifdef DEBUG
+    WRITE(stdout,'(     2A)')'Hello from ', trim(myname_)
+#endif
+
+     e  = 611.20*exp((17.67*Td)/(Td + 243.5)); 
+     q2 = (0.622 * e)/(pres - (0.378 * e)); 
+     q2 = q2 * 1000.0
+
+  endfunction
+  !EOC
+  !-----------------------------------------------------------------------------!
+  !BOP
+  !
+  ! !IROUTINE:  td
+  !
+  ! !DESCRIPTION: Esta funcao calcula a Temperatura do Ponto de Orvalho [C] 
+  !               
+  !
+  !\\
+  !\\
+  ! !INTERFACE:
+
+  function td(t,rh)
+     implicit none
+
+     !
+     ! !INPUT PARAMETERS:
+     !
+
+     real, intent(in) :: rh ! Umidade Relativa [-]
+     real, intent(in) :: t   ! Tempeatura do Ar [C]
+
+     !
+     ! !OUTPUT PARAMETERS:
+     ! 
+
+     real :: td ! Temperatura do Ponto de Orvalho [C]
+
+     !
+     !
+     ! !REVISION HISTORY: 
+     !  19 Feb 2013 - J. G. de Mattos - Initial Version
+     !
+     ! !SEE ALSO: 
+     !     Bolton, D., The computation of equivalent potential temperature, 
+     !                  Monthly Weather Review, 108, 1046-1053, 1980. 
+     !      
+     !     
+     !
+     !EOP
+     !-----------------------------------------------------------------------------!
+     !BOC
+     !
+     
+     real :: e, es
+
+    character(len=1024),parameter :: myname_=trim(myname)//'::td'
+
+#ifdef DEBUG
+    WRITE(stdout,'(     2A)')'Hello from ', trim(myname_)
+#endif
+
+
+     es = 6.112 * exp((17.67 * t)/(t + 243.5)); 
+     e = es * ( rh / 100.0 ); 
+     Td = log(e/6.112)*243.5/(17.67-log(e/6.112)); 
+
+  endfunction
+  !EOC
+  !-----------------------------------------------------------------------------!
+  !BOP
+  !
+  ! !IROUTINE:  rh
+  !
+  ! !DESCRIPTION: Esta função calcula a umidade relativa [-] a partir da
+  !               razão de mistura [kg/kg], da pressao de vapor de saturacao [hPa] e
+  !               da pressão atmosferica [hPa].
+  !\\
+  !\\
+  ! !INTERFACE:
+
+
+  function rh(w,es,pres)
+    Implicit None
+
+    !
+    ! !INPUT PARAMETERS:
+    !
+
+    real, intent(in) :: w    ! Razão de Mistura [-]
+    real, intent(in) :: es   ! Presão de Vapor Saturada [Pa]
+    real, intent(in) :: pres ! Presão Atmosférica [Pa]
+
+    !
+    ! !OUTPUT PARAMETERS:
+    !
+
+    real :: rh  ! Umidade Relativa [-]
+
+    !
+    !
+    ! !REVISION HISTORY: 
+    !  19 Feb 2013 - J. G. de Mattos - Initial Version
+    !
+    !
+    !EOP
+    !-----------------------------------------------------------------------------!
+    !BOC
+    !
+
+    REAL :: ws, eps
+
+    character(len=1024),parameter :: myname_=trim(myname)//'::rh'
+
+#ifdef DEBUG
+    WRITE(stdout,'(     2A)')'Hello from ', trim(myname_)
+#endif
+
+    eps = Rd / Rv
+    ws  = (eps*es) / (pres - es);
+    rh  = (w/ws)
+
+    !  mask to rh > 100 %
+    IF(rh .GT. 1.0) rh = 1.0
+
+  end function
+  !EOC
+  !
+  !-----------------------------------------------------------------------------!
+  !BOP
+  !
+  ! !IROUTINE:  Tv
+  !
+  ! !DESCRIPTION: Esta função calcula a temperatura virtual [K] a partir da
+  !               temperatura do ar [K] e da Umidade especícia [Kg/Kg]
+  !
+  !\\
+  !\\
+  ! !INTERFACE:
+
+  function Tv(t,q)
+
+    Implicit None
+
+    !
+    ! !INPUT PARAMETERS:
+    !
+
+    real, intent(in) :: t ! temperatura do ar [K]
+    real, intent(in) :: q ! Umidade Específica [kg/Kg]
+
+    !
+    ! !OUTPUT PARAMETERS:
+    !
+
+    real :: tv ! temperatura virtural [K]
+
+    !
+    !
+    ! !REVISION HISTORY: 
+    !  19 Feb 2013 - J. G. de Mattos - Initial Version
+    !
+    !
+    !EOP
+    !-----------------------------------------------------------------------------!
+    !BOC
+    !
+
+    character(len=1024),parameter :: myname_=trim(myname)//'::tv'
+
+#ifdef DEBUG
+    WRITE(stdout,'(     2A)')'Hello from ', trim(myname_)
+#endif
+
+    tv = t * ( 1 + 0.61 * ( q / ( 1 - q ) ) ) 
+
+  end function
+  !EOC
+  !
+  !-----------------------------------------------------------------------------!
+
+End Module SCAM_MetForm
