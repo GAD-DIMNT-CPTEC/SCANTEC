e78c5565d48a7f5425ba6b4b3a3391d813c8cea9
carlos.frederico <carlos.frederico@f4d869c9-3b79-40b0-af35-19216b5cd462>
2016-10-19 13:06:04 +0000

    Atualização da rotina m_TQ062L028.f90 a partir de m_agcm.f90.
    Esta atualização foi feita porque existia uma discrepância entre as
    rotinas m_agcm.f90 m_TQ062L028.f90. Ao se tentar avaliar o MCGA na
    resolução TQ0062L028, problemas foram encontrados na leitura dos
    arquivos grib. O problema foi resolvido utilizando a forma de leitura do grib contida na rotina
    m_agcm.f90 a aplicando na rotina m_TQ062L028.f90.

--- trunk/SCAMTEC/core/m_TQ062L028.f90
+++ trunk/SCAMTEC/core/m_TQ062L028.f90
@@ -182,138 +182,21 @@ CONTAINS
     WRITE(stdout,'(     2A)')'Hello from ', myname_
 #endif
 
-    !                    http://rda.ucar.edu/docs/formats/grib/gribdoc/
-    !
-    !                    Grib Section 2 - Grid description section
-    !
-
     TQ062L028_struc%gridDesc     = 0 
 
-                                          !Octet No.	Contents
-    TQ062L028_struc%gridDesc( 1) =       4!     6    Data representation type (see Code table 6)
-                                          !
-                                          !-------------------------------------------------------------------------------
-                                          !       Table 6
-                                          ! 0 Latitude/longitude grid - equidistant cylindrical or Plate Carrée projection
-                                          ! 1 Mercator projection
-                                          ! 2 Gnomonic projection
-                                          ! 3 Lambert conformal, secant or tangent, conic or bi-polar, projection
-                                          ! 4 Gaussian latitude/longitude grid
-                                          ! 5 Polar stereographic projection
-                                          ! ...
-                                          !
-                                          !-------------------------------------------------------------------------------
-                                          ! 7-32  Grid definition (according to data representation type - octet 6 above)
-                                          !-------------------------------------------------------------------------------
-                                          !
-    TQ062L028_struc%gridDesc( 2) =     192!  7- 8    Ni - number of points along a parallel
-    TQ062L028_struc%gridDesc( 3) =      96!  9-10    Nj - number of points along a meridian
-    TQ062L028_struc%gridDesc( 4) =  88.572! 11-13    La1 - latitude of first grid point
-    TQ062L028_struc%gridDesc( 5) =     0.0! 14-16    Lo1 - longitude of first grid point
+    TQ062L028_struc%gridDesc( 1) = 4         !Input grid type (4=Gaussian)
+    TQ062L028_struc%gridDesc( 2) = 192       !Number of points on a lat circle
+    TQ062L028_struc%gridDesc( 3) = 96       !Number of points on a meridian
+    TQ062L028_struc%gridDesc( 4) = 88.572  !Latitude of origin
+    TQ062L028_struc%gridDesc( 5) = 0.0       !Longitude of origin
+    TQ062L028_struc%gridDesc( 6) = 128       !8 bits (1 byte) related to resolution
+                                        !(recall that 10000000 = 128), Table 7
+    TQ062L028_struc%gridDesc( 7) = -88.572 !Latitude of extreme point
+    TQ062L028_struc%gridDesc( 8) = 358.125    !Longitude of extreme point
+    TQ062L028_struc%gridDesc( 9) = 1.875     !N/S direction increment
+    TQ062L028_struc%gridDesc(10) = 48       !(Gaussian) # lat circles pole-equator
+    TQ062L028_struc%gridDesc(20) = 0.0  
 
-                                          ! --------------------------------------------------------------
-    TQ062L028_struc%gridDesc( 6) =     128!    17    Resolution and component flags (see Code table 7)
-                                          ! --------------------------------------------------------------
-                                          !
-                                          ! Bit No.   Value  Meaning
-        	                                 !    1        0    Direction increments not given
-                                          !             1    Direction increments given
-                                          !
-        	                                 !    2        0    Earth assumed spherical with radius 6367.47 km
-                                          !             1    Earth assumed oblate spheroidal with size as 
-                                          !                  determined by IAU in 1965 (6378.160 km, 6356.775 km, f=1/297.0)
-                                          !
-        	                                 !   3-4            Reserved
-                                          !
-                                          !    5        0    Resolved u- and v-components of vector quantities relative to easterly and northerly directions
-                                          !             1    Resolved u- and v-components of vector quantities relative to the defined grid in the direction
-                                          !                  of increasing x and y (or i and j) coordinates respectively
-                                          !
-                                          !   6-8       0    Reserved - set to zero
-                                          !    
-
-    TQ062L028_struc%gridDesc( 7) = -88.572! 18-20    La2 - latitude of last grid point
-    TQ062L028_struc%gridDesc( 8) = 358.125! 21-23    Lo2 - longitude of last grid point
-    TQ062L028_struc%gridDesc( 9) =   1.875! 24-25    Di - i direction increment
-    TQ062L028_struc%gridDesc(10) =      48! 26-27    N - number of parallels between a pole and the equator
-
-                                          ! --------------------------------------------------------------
-    TQ062L028_struc%gridDesc(11) =       0!    28	   Scanning mode (flags - see Code table 8)
-                                          ! --------------------------------------------------------------
-                                          !    WMO Code table 8 - Scanning mode
-                                          !
-                                          ! Bit No.   Value  Meaning
-                                          !    1	     0    Points scan in +i direction
-        	                                 !             1    Points scan in -i direction
-                                          !    2        0    Points scan in -j direction
-        	                                 !             1    Points scan in +j direction
-                                          !    3        0    Adjacent points in i direction are consecutive
-        	                                 !             1    Adjacent points in j direction are consecutive
-                                          !  4-8        0	 Reserved
-                                          !
-                                          ! NOTES:
-                                          !
-                                          !   i direction: west to east along a parallel, or left to right along an X-axis.
-                                          !   j direction: south to north along a meridian, or bottom to top along a Y-axis.
-                                          !   -----------------------------------------------
-                                          !
-                                          ! Values to GDS(11)
-                                          !  Value  Result
-                                          !     0    0 0 0
-                                          !    32    0 0 1
-                                          !    64    0 1 0
-                                          !    96    0 1 1
-                                          !   128    1 0 0
-                                          !   160    1 0 1
-                                          !   192    1 1 0
-                                          !   224    1 1 1
-                                          !
-
-    TQ062L028_struc%gridDesc(12) =       0! 29-32    Set to zero (reserved)
-                                          !
-                                          !-------------------------------------------------------------------------------
-                                          !
-                                          ! 33-42 Extensions of grid definition for rotation or stretching of the coordinate system
-                                          !        or Lambert conformal projection or Mercator projection
-                                          !-------------------------------------------------------------------------------
-                                          !
-    TQ062L028_struc%gridDesc(13) =       0! 33-35    Latitude of the southern pole in millidegrees (integer)
-                                          !          Latitude of pole of stretching in millidegrees (integer)
-    TQ062L028_struc%gridDesc(14) =       0! 36-38    Longitude of the southern pole in millidegrees (integer)
-                                          !          Longitude of pole of stretching in millidegrees (integer)
-    TQ062L028_struc%gridDesc(15) =       0! 39-42    Angle of rotation (represented in the same way as the reference value)
-                                          !          Stretching factor (representation as for the reference value)
-                                          !
-                                          !-------------------------------------------------------------------------------
-                                          !
-                                          ! 33-44 Extensions of grid definition for space view perspective projection
-                                          ! 33-52 Extensions of grid definition for stretched and rotated coordinate system
-                                          !
-                                          !-------------------------------------------------------------------------------
-                                          !
-    TQ062L028_struc%gridDesc(16) =       0! 43-45    Latitude of pole of stretching in millidegrees (integer)
-    TQ062L028_struc%gridDesc(17) =       0! 46-48    Longitude of pole of stretching in millidegrees (integer)
-    TQ062L028_struc%gridDesc(18) =       0! 49-52    Stretching factor (representation as for the reference value)
-
-                                          !-------------------------------------------------------------------------------
-    TQ062L028_struc%gridDesc(19) =       0! PV	List of vertical coordinate parameters 
-                                          ! (length = NV x 4 octets); if present, then PL = 4NV + PV
-    TQ062L028_struc%gridDesc(20) =       0! PL	List of numbers of points in each row 
-                                          ! (length = NROWS x 2 octets, where NROWS is the total number of rows defined within the grid description)
-                                          !
-                                          ! NOTES:
-                                          !
-                                          ! 1- Vertical coordinate parameters are used in association with hybrid vertical coordinate systems.
-
-                                          ! 2- Hybrid systems, in the context, employ a means of representing vertical coordinates in terms
-                                          !    of a mathematical combination of pressure and sigma coordinates. When used in conjunction with
-                                          !    a surface pressure field and an appropriate mathematical expression, the vertical coordinate 
-                                          !    parameters may be used to interpret the hybrid vertical coordinate.
-                                          !
-                                          ! 3- Each vertical coordinate parameter is represented in 4 octets, using the scheme for representing
-                                          !    floating point numbers described in the Regulations.
-                                          !
-                                          !-------------------------------------------------------------------------------
 
     TQ062L028_struc%npts = TQ062L028_struc%gridDesc(2)*TQ062L028_struc%gridDesc(3)
 
@@ -365,8 +248,7 @@ CONTAINS
     integer :: npts
     integer :: nx
     integer :: ny
-    character(len=4), dimension(28) :: pds5
-    integer, dimension(28) :: pds7
+    integer, dimension(20) :: pds5, pds7
     logical*1, dimension(:,:), allocatable :: lb, lb2
     real, dimension(:,:), allocatable :: f
     real, dimension(:,:), allocatable :: f2
@@ -390,63 +272,87 @@ CONTAINS
     !
 
 
-    pds5( 1:10) = (/'UMES','UMES','UMES','TEMP','TEMP','TEMP','TEMP','PSNM','AGPL','ZGEO'/)
-    pds7( 1:10) = (/  925 ,  850 ,  500 ,  925 ,  850 ,  500 ,  250 ,  000 ,  000 ,  850 /)
+    lugb = 1
+    lubi = 0
+    j    = 0
+    jpds = -1 
+    !          Q   Q   Q   T   T   T   T  P   A   Z   Z   Z   U   U   U   V   V   V  PRE PRC
+    pds5 = (/ 51, 51, 51, 11, 11, 11, 11, 2, 54,  7,  7,  7, 33, 33, 33, 34, 34, 34, 61, 63/) !parameter
+    pds7 = (/925,850,500,925,850,500,250,000,000,850,500,250,850,500,250,850,500,250,000,000/) !htlev2
 
-    pds5(11:20) = (/'ZGEO','ZGEO','UVEL','UVEL','UVEL','VVEL','VVEL','VVEL','PREC','PRCV'/)
-    pds7(11:20) = (/  500 ,  250 ,  850 ,  500 ,  250 ,  850 ,  500 ,  250 ,  000 ,  000 /)
-
-    pds5(21:28) = (/'TEMS','Q02M','TP2M','USSL','UZRS','UZDS','U10M','V10M'/)
-    pds7(21:28) = (/  000 ,  002 ,  002 ,  000 ,  000 ,  000 ,  010 ,  010 /)
+    allocate(lb2(TQ062L028_struc%npts,scamtec%nvar))
+    allocate(lb(TQ062L028_struc%npts,size(pds5)))
+    allocate(f(TQ062L028_struc%npts,size(pds5)))
 
   
+    inquire (file=trim(fname), exist=file_exists)
     
-    do i=1, len_trim(fname)	
+    do i=1, 300	
 		
-		    if(trim(fname(i:i+6)) .eq. 'GPOSERA')then
+		    if(trim(fname(i:i+6)) .eq. 'GPOSNMC')then
 			    dataini=trim(fname(i+7:i+16))
 			    datafinal=trim(fname(i+17:i+26))
 		    endif			
 		
-    enddo 
+	    enddo 
 	   	
-!   if(dataini .eq. datafinal)then
-!	    y=size(pds7)-2
-!   else
-	    y=size(pds7)
-!   endif
+	    if(dataini .eq. datafinal)then
+		    y=size(pds5)-2
+	    else
+		    y=size(pds5)
+	    endif
+	    	    	    
+    
+    do iv = 1, y!size(pds5)
+             
+       if (file_exists) then 
 
+          jpds(5) = pds5(iv)
+          jpds(7) = pds7(iv)
 
-   inquire (file=trim(fname), exist=file_exists)
-   if (file_exists) then
+          lugb    = lugb + iv
 
-      grb%file = trim(fname)
-      call OpenGrib(grb,iret)
-      if (iret .ne. 0) then
-         stat = iret
-         return
-      endif
+          call baopenr(lugb,fname,iret)
 
-      allocate(f(TQ062L028_struc%npts,size(pds7)))
-      allocate(lb(TQ062L028_struc%npts,size(pds7)))
-      lb = .true.
+          if(iret.eq.0) then
+             call getgb(lugb,lubi,TQ062L028_struc%npts,j,jpds,jgds,kf,k,kpds, &
+                  gridDesc,lb(:,iv),f(:,iv),gbret)
 
-      do iv=1,y
-         call ReadGrib(grb,pds5(iv),pds7(iv),f(:,iv))
-         where(f(:,iv).eq.grb%undef) lb(:,iv) = .false.
-      enddo
+             if (gbret.ne.0)then
+                stat = gbret
+                call perr(myname_,'getgb("'//     &
+                          trim(fname)//'")',gbret &
+                         )
+                return
+             endif
 
-      call CloseGrib(grb)
+          else
+             stat = iret
+             call perr(myname_,'baopenr("'//     &
+                       trim(fname)//'")',iret &
+                      )
+             return
+          endif
 
-   else
-      stat = -1
+          call baclose(lugb,jret)
+          if(jret.ne.0) then
+            stat = jret
+            call perr(myname_,'deallocate()',jret)
+            return
+          endif
+       else
+          stat = -1
 
-      call perr(myname_,'File Not Found: '//trim(fname),stat)
-      return
+          deallocate(f)
+          deallocate(lb)
+          
+          call perr(myname_,'File Not Found: '//trim(fname),stat)
+          return
 
-   endif
+       endif
 
- 
+    enddo
+
     !
     ! Convertendo as Variaveis para as utilizadas no SCAMTEC
     ! * A lista de variaveis e as unidades utilizadas podem ser
@@ -484,24 +390,13 @@ CONTAINS
     f2(:,19) = f(:,17); lb2(:,19) = lb(:,17)             ! Vvel @ 500 hPa [m/s]
     f2(:,20) = f(:,18); lb2(:,20) = lb(:,18)             ! Vvel @ 250 hPa [m/s]
            
-!    if (y .eq. size(pds5)-2)then
-!    f2(:,21) = 0.0; lb2(:,21) = .false.             ! TOTAL PRECIPITATION @ 1000 hPa [kg/m2/day]     
-!    f2(:,22) = 0.0; lb2(:,22) = .false.             ! CONVECTIVE PRECIPITATION @ 1000 hPa [kg/m2/day]   
-!    else
+    if (y .eq. size(pds5)-2)then
+    f2(:,21) = 0.0; lb2(:,21) = .false.             ! TOTAL PRECIPITATION @ 1000 hPa [kg/m2/day]     
+    f2(:,22) = 0.0; lb2(:,22) = .false.             ! CONVECTIVE PRECIPITATION @ 1000 hPa [kg/m2/day]   
+    else
     f2(:,21) = f(:,19); lb2(:,21) = lb(:,19)             ! TOTAL PRECIPITATION @ 1000 hPa [kg/m2/day]     
     f2(:,22) = f(:,20); lb2(:,22) = lb(:,20)             ! CONVECTIVE PRECIPITATION @ 1000 hPa [kg/m2/day]   
-!    endif
-
-    f2(:,23) = f(:,21); lb2(:,23) = lb(:,21)  ! SURFACE ABSOLUTE TEMPERATURE [K]               
-    f2(:,24) = f(:,22)*1000.0; lb2(:,24) = lb(:,22)  ! SPECIFIC HUMIDITY AT 2-M FROM SURFACE   (G/KG)
-    f2(:,25) = f(:,23); lb2(:,25) = lb(:,23)  ! TEMPERATURE AT 2-M FROM SURFACE         (K)    
-    f2(:,26) = f(:,24); lb2(:,26) = lb(:,24)  ! SOIL WETNESS OF SURFACE                 (0-1)  
-    f2(:,27) = f(:,25); lb2(:,27) = lb(:,25)  ! SOIL WETNESS OF ROOT ZONE               (0-1)  
-    f2(:,28) = f(:,26); lb2(:,28) = lb(:,26)  ! SOIL WETNESS OF DRAINAGE ZONE           (0-1)  
-    f2(:,29) = f(:,27); lb2(:,29) = lb(:,27)  ! 10 METRE U-WIND COMPONENT               (M/S)  
-    f2(:,30) = f(:,28); lb2(:,30) = lb(:,28)  ! 10 METRE V-WIND COMPONENT               (M/S)  
-
-
+    endif
     DeAllocate(lb)
     DeAllocate(f)
 
