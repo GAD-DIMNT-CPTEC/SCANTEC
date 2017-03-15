18a498dd4c1daf4b89c5997ff7825686b3fd9c90
arletis.roque <arletis.roque@f4d869c9-3b79-40b0-af35-19216b5cd462>
2017-03-15 18:41:55 +0000

    Atualizando os arquivos que realizam a avaliação orientada a objeto.

--- trunk/SCAMTEC/core/m_mode_singleAttrib.f90
+++ trunk/SCAMTEC/core/m_mode_singleAttrib.f90
@@ -27,13 +27,11 @@ MODULE m_mode_singleAttrib
    type mark	! Estructura para crear listas enlazadas de puntos		
       integer			:: x, y
       type(mark), pointer	:: next => null()
-   end type
-      !type(mark), pointer	:: points
+   end type      
       
    type point	! Estructura para guardar arreglos de puntos
       integer			:: x, y
-   end type    
-      !type(point), allocatable		:: pts_per(:), total_pts(:)  
+   end type          
 
    type attrs	! Estructura para guardar los atributos de los objetos
       integer			:: id, area, xcent, ycent, perimeter
@@ -72,10 +70,7 @@ MODULE m_mode_singleAttrib
 	   suma = suma + total_pts(i)%x
 	 Enddo
 
-	 xcent = INT(suma/area)
-	 !print*, 'suma', suma, 'area', area, 'xcent', xcent
-	 !xcent = INT(temp)
-	 !print*, 'xcent', xcent
+	 xcent = INT(suma/area)	 
 	 return
       End Subroutine
     !**************************************************************************************************************************************
@@ -95,10 +90,7 @@ MODULE m_mode_singleAttrib
 	   suma = suma + total_pts(i)%y
 	 Enddo
 
-	 ycent = INT(suma/area)
-	 !print*, 'suma', suma, 'area', area, 'xcent', ycent
-	 !xcent = INT(temp)
-	 !print*, 'xcent', xcent
+	 ycent = INT(suma/area)	 
 	 return
       End Subroutine
     !**************************************************************************************************************************************
@@ -111,33 +103,22 @@ MODULE m_mode_singleAttrib
 
 	Implicit None
 	! INPUT PARAMETERS:
-	type(point), pointer, intent(in)			:: pts_per(:)
+	type(point), pointer, intent(in)	:: pts_per(:)
         integer, intent(in)			:: xcent, ycent, perimeter	
-	!OUTPUT PARAMETERS:
-	!type(point), pointer, intent(out)	:: pts_per(:)
+	!OUTPUT PARAMETERS:	
 	real, intent(out)			:: angle
-
-	!type(point), pointer	:: pts_per(:)
+	
 	integer			:: i, j
 	real, parameter   	:: pi=3.141592654
 	real			:: sumUp, sumDown
 
 	sumUp=0.0
 	sumDown=0.0
-
-	!allocate(pts_per(perimeter))
-
-	!Do while (associated(per_points))
-	   do i=1, perimeter
-	      !pts_per(i)%x = per_points%x
-	      !pts_per(i)%y = per_points%y
-
-	      sumUp = sumUp + (pts_per(i)%x - xcent) * (pts_per(i)%y - ycent)
-	      sumDown = sumDown + (pts_per(i)%x - xcent)**2 - (pts_per(i)%y - ycent)**2
 	
-	      !per_points => per_points%next
-	   enddo
-        !Enddo
+        do i=1, perimeter
+	  sumUp = sumUp + (pts_per(i)%x - xcent) * (pts_per(i)%y - ycent)
+	  sumDown = sumDown + (pts_per(i)%x - xcent)**2 - (pts_per(i)%y - ycent)**2
+	enddo        
 
 	angle = 0.5*ATAN2(2*sumUp,sumDown)
 
@@ -175,8 +156,7 @@ MODULE m_mode_singleAttrib
 	minor_up=0
 
 	! calculate the maximum and minimum distances from semiaxes to the perimeter
-	Do i=1, perimeter
-	   
+	Do i=1, perimeter	   
 	   dist =  (pts_pos(i)%x - xcent)*SIN(angle) - (pts_pos(i)%y - ycent)*COS(angle)
 	   vmajor = (pts_pos(i)%y - ycent) - TAN(angle)*(pts_pos(i)%x - xcent)
 	   
@@ -205,8 +185,7 @@ MODULE m_mode_singleAttrib
 	      if(dist .GT. minor_down) then
 		 minor_down = dist
 	      endif
-	   Endif
-	   
+	   Endif	   
 	Enddo
 
 	major_axis = major_up + major_down
@@ -224,10 +203,9 @@ MODULE m_mode_singleAttrib
 
 
 
-      !**************************************************************************************************************************************
+    !**************************************************************************************************************************************
      ! Quickhull algorithm to find the convex hulls of the objects
-      Subroutine quick_hull(array_pts, npoints, hull)
-	
+      Subroutine quick_hull(array_pts, npoints, hull)	
          Implicit None
 	 ! INPUT PARAMETERS:
 	 type(point), pointer, intent(in)	:: array_pts(:)
@@ -236,105 +214,65 @@ MODULE m_mode_singleAttrib
 	 type(mark), pointer, intent(out)	:: hull
 
 	 type(point), pointer			:: AB(:), temp, left_set(:), right_set(:), array_aux, aux_AB1, aux_AB2
-
 	 type(mark), pointer			:: hull_temp => null()
 	 type(mark), pointer			:: aux => null()
 
 	 integer				:: i, j, k, positions, nleft, nright, xmin, ymin, xmax, ymax, minpos, maxpos, side, tempAx, tempAy, tempBx, tempBy, tempx, tempy, size_line
 
-
 	 xmin=9999
-	 xmax=0
-	 
-	 
-	 !print*, 'array_pts(:)', array_pts
-	 !Loop para buscar los xmin y xmax
-
-!         maxpos = maxloc(array_pts(:)%x)
-!         minpos = minloc(array_pts(:)%x)        
-
+	 xmax=0	 
          
 	 Do i=1, npoints	    
 	    If (array_pts(i)%x .LT. xmin) then
 	       xmin = array_pts(i)%x
 	       ymin = array_pts(i)%y
-	       minpos = i
-	       !print*
-	       !print*, 'xmin', xmin, 'ymin', ymin
-		!print*, ' minpos', minpos
+	       minpos = i	       
 	    Endif
 
 	    If (array_pts(i)%x .GT. xmax) then
 	       xmax = array_pts(i)%x
 	       ymax = array_pts(i)%y
-	       maxpos = i
-	       !print*
-	       !print*, 'xmax', xmax, 'ymax', ymax
-		!print*, 'maxpos', maxpos
+	       maxpos = i	       
 	    Endif
 	 Enddo
-
-	 !print*	 
-	 !print*, ' minpos final', minpos, '    maxpos final', maxpos
-
-	 ! pts extremos de las x
+	 
 	 allocate(AB(2))
 
 	 AB(1)%x = array_pts(minpos)%x
 	 AB(1)%y = array_pts(minpos)%y
 	 AB(2)%x = array_pts(maxpos)%x
-	 AB(2)%y = array_pts(maxpos)%y
-	 
-
-	 !print*, 'array_pts(minpos)', array_pts(minpos)
+	 AB(2)%y = array_pts(maxpos)%y	
 	 
          tempx = array_pts(minpos)%x
 	 tempy = array_pts(minpos)%y	 
 	 array_pts(minpos)%x = array_pts(1)%x
 	 array_pts(minpos)%y = array_pts(1)%y	 
 	 array_pts(1)%x = tempx	
-	 array_pts(1)%y = tempy
-
-	 !print*, 'tempx', tempx, 'tempy', tempy
-	 !print*, 'array_pts(minpos)', array_pts(minpos)
-	 !print*, 'array_pts(1)', array_pts(1) 
+	 array_pts(1)%y = tempy	
 	 
 	 tempx = array_pts(maxpos)%x
 	 tempy = array_pts(maxpos)%y
 	 array_pts(maxpos)%x = array_pts(2)%x
 	 array_pts(maxpos)%y = array_pts(2)%y
          array_pts(2)%x = tempx
-	 array_pts(2)%y = tempy
-
-	 !print*
-	 !print*, 'array_pts', array_pts
+	 array_pts(2)%y = tempy	 
 
 	 ! guardo A y B como parte de la convex hull
 	 allocate(aux)
 	 allocate(aux%next)
 	 aux%next => hull_temp
 	 hull_temp => aux
-	 hull_temp%x = AB(1)%x;
-         hull_temp%y = AB(1)%y;
-
-
+	 hull_temp%x = AB(1)%x
+         hull_temp%y = AB(1)%y
 
 	 allocate(aux)
 	 allocate(aux%next)
 	 aux%next => hull_temp
 	 hull_temp => aux
-	 hull_temp%x = AB(2)%x;
-         hull_temp%y = AB(2)%y;
-
-	 !do while(associated(aux))
-	    !print*, 'A aux%x', aux%x
-	    !print*, 'A aux%y', aux%y
-	    !aux => aux%next
-	 !enddo
+	 hull_temp%x = AB(2)%x
+         hull_temp%y = AB(2)%y	 
 	 
-	 ! hallo los conjuntos a ambos lados del segmento AB
-	 !print*
-	 !print*, 'hallo los conjuntos a ambos lados del segmento AB'
+	 ! hallo los conjuntos a ambos lados del segmento AB	 
 	 aux_AB1 => AB(1)
 	 aux_AB2 => AB(2)
 
@@ -344,15 +282,7 @@ MODULE m_mode_singleAttrib
 	 Do i=3, npoints
             array_aux => array_pts(i)	 
    
-	    !print*
-	    !print*, '   Calculando nleft e nright --  i', i, '    array_pts(i)', array_aux
-	    !print*
-
-	    !print*, 'A', A, 'B', B
-	    call orientation(aux_AB1, aux_AB2, array_aux, positions)
-	    
-	    !print*
-	    !print*, '   orientation - positions', positions
+	    call orientation(aux_AB1, aux_AB2, array_aux, positions)	    
 
 	    if (positions .GT. 0) then  
 	       nleft = nleft + 1
@@ -361,32 +291,17 @@ MODULE m_mode_singleAttrib
 	    if (positions .LT. 0) then  
 	       nright = nright + 1
 	    endif
-	 Enddo
-
-	 !print*
-	 !print*, 'nleft', nleft, 'nright', nright
-
-!	 if (nleft.ne.0)allocate(left_set(nleft))
-!	 if (nright.ne.0)allocate(right_set(nright))
+	 Enddo	 
 
 	 allocate(left_set(nleft))
 	 allocate(right_set(nright))
 
 	 j=0
-	 k=0
-
-	 !print*
-	 !print*, 'hallar left_set y right_set'
+	 k=0	
 	 Do i=3, npoints
-	    array_aux => array_pts(i)
-	    !print*
-	    !print*, '   Calculando j e k --- i', i, '    array_pts(i)', array_aux
-	    !print*
-	    !print*, 'A', A, 'B', B
+	    array_aux => array_pts(i)	    
 	    call orientation(aux_AB1, aux_AB2, array_aux, positions)
 
-	    !print*
-	    !print*, '   orientation -- positions1', positions
 	    if (positions .GT. 0) then  
 	       j=j+1
 	       left_set(j) = array_pts(i)
@@ -398,18 +313,10 @@ MODULE m_mode_singleAttrib
 	    endif
 	 Enddo
 
-	 !print*
-	 !print*, 'j', j, 'k', k
-	 !if (nleft.ne.0) print*, 'left_set', left_set
-	 !print*
-	 !if (nright.ne.0)print*, 'right_set', right_set
-
-
 	 call Hull_Set(aux_AB1, aux_AB2, left_set, nleft, hull_temp)
 	 call Hull_Set(aux_AB1, aux_AB2, right_set, nright, hull_temp)
 
 	 hull => hull_temp
-
 	 return
       End Subroutine
     !**************************************************************************************************************************************
@@ -419,7 +326,6 @@ MODULE m_mode_singleAttrib
     !**************************************************************************************************************************************
      ! determina los ptos que pertenecen a la convex hull
       Recursive Subroutine Hull_Set(A, B, set, array_size, hull_temp) 
-
 	 Implicit None
 	 ! INPUT PARAMETERS:
 	 type(point), pointer, intent(in)	:: A, B, set(:)
@@ -437,8 +343,7 @@ MODULE m_mode_singleAttrib
 
 	 hull_temp2 => hull_temp
 
-	 ! compruebo el tamaño del arreglo
-	 !print*, '   array_size', array_size
+	 ! compruebo el tamaño del arreglo	 
 	 if (array_size .EQ. 0) then
 	    return
 	 endif
@@ -465,9 +370,7 @@ MODULE m_mode_singleAttrib
 	       distmax = dist
 	       furthest_point = i
 	    endif
-	 Enddo
-
-	 !print*, 'distmax', distmax, 'furthest_point', furthest_point   
+	 Enddo	 
 	 	  
 	 ! guardar el pto perteneciente a la convex hull
 	 allocate(aux) 
@@ -497,8 +400,6 @@ MODULE m_mode_singleAttrib
 	    endif
          Enddo
 
-	 !print*, 'nAP', nAP, '  nPB', nPB
-
 	 allocate(leftset_AP(nAP), leftset_PB(nPB))
 	 j=0
 	 k=0
@@ -519,18 +420,13 @@ MODULE m_mode_singleAttrib
 	    endif
 	 Enddo
 
-	 !print*, 'jhull', j, '   khull', k
-	 !print*, 'leftset_AP', leftset_AP, '   leftset_PB', leftset_PB
-
 	 call Hull_Set(A, set_aux2, leftset_AP, nAP, hull_temp2)
 	 call Hull_Set(set_aux2, B, leftset_PB, nPB, hull_temp2)
 
 	 hull_temp => hull_temp2
-
 	 return
       End Subroutine
     !**************************************************************************************************************************************
-
 
 
 
@@ -542,13 +438,7 @@ MODULE m_mode_singleAttrib
 	 type(point), pointer, intent(in)	:: p1, p2, p3
 	 integer, intent(out) 			:: size_line
 
-	 !print*
-	 !print*, '   p1', p1
- 	 !print*, '   p2', p2
-	 !print*, '   p3', p3
-
-	 size_line = (p2%x - p1%x)*(p3%y - p1%y) - (p2%y - p1%y)*(p3%x - p1%x)	 
-	 !print*, '   size_line', size_line 
+	 size_line = (p2%x - p1%x)*(p3%y - p1%y) - (p2%y - p1%y)*(p3%x - p1%x)		 
          return
       End Subroutine
     !**************************************************************************************************************************************
@@ -574,7 +464,7 @@ MODULE m_mode_singleAttrib
 	 if (pdist .LT. 0) then
 	    pdist = (-1)*pdist
 	 endif
-         !print*, '   distance', pdist
+         
          return
       End Subroutine
     !**************************************************************************************************************************************
@@ -590,18 +480,14 @@ MODULE m_mode_singleAttrib
 	integer, intent(in)			:: low, high
 
 	integer					:: pivot, i, j, size_line, tempx, tempy
-	type(point), pointer			:: temp, arr_aux, arr_aux2, arr_aux3, new_arr, arr_sort(:)
-	!print*, 'low, high', low, high
+	type(point), pointer			:: temp, arr_aux, arr_aux2, arr_aux3, new_arr, arr_sort(:)	
 
 	if (low .lT. high) then
 	   pivot = low
 	   i = low
-	   j = high
-	   !print*
-	   !print*, '   i, j', i, j
+	   j = high	  
 
-           do while (i .LT. j)
-	      !print*, '     do while (i .LT. j)', i, j
+           do while (i .LT. j)	      
 	      arr_aux => arr(1)
 	      arr_aux2 => arr(pivot)
 	      arr_aux3 => arr(i)
@@ -611,7 +497,6 @@ MODULE m_mode_singleAttrib
 	         new_arr => arr(i)
 	         call orientation(arr_aux, arr_aux2, new_arr, size_line)
 	      enddo
-	      !print*, '     new i', i	
 	      
 	      arr_aux3 => arr(j)
 	      call orientation(arr_aux, arr_aux2, arr_aux3, size_line)
@@ -620,7 +505,6 @@ MODULE m_mode_singleAttrib
 	         new_arr => arr(j)
 	         call orientation(arr_aux, arr_aux2, new_arr, size_line)
 	      enddo
-	      !print*, '     new j', j
 
 	      if (i .LT. j) then	         
 	         tempx = arr(i)%x
@@ -631,7 +515,6 @@ MODULE m_mode_singleAttrib
 	         
 	         arr(j)%x = tempx
 		 arr(j)%y = tempy
-		 !print*, 'new arr', arr
 	      endif	      
 	   enddo
 	   
@@ -642,11 +525,9 @@ MODULE m_mode_singleAttrib
 	   arr(pivot)%y = arr(j)%y
 	   
 	   arr(j)%x = tempx
-	   arr(j)%y = tempy
-	   !print*, 'new new arr', arr	   
+	   arr(j)%y = tempy   
 
-	   call quick_sort(arr, low, j-1) 
-	   !print*, 'high', high          
+	   call quick_sort(arr, low, j-1) 	           
 	   call quick_sort(arr, j+1, high)
 	endif        
 	
@@ -666,8 +547,7 @@ MODULE m_mode_singleAttrib
 	 integer, intent(out)			:: area_hull
 
 	 integer	:: ymin, ymax, xmax, xmin, xleft, xright, xmayor, xmenor, x, y, i, j, temp, k
-
-	 !area_hull = 0
+	 
 	 ymin = 9999
 	 ymax = 0
 	 xmin = 9999
@@ -690,16 +570,14 @@ MODULE m_mode_singleAttrib
 		xmax = set(i)%x
 	    Endif
 	 Enddo
-	 !print*, 'ymin, ymax, xmax, xmin', ymin, ymax, xmax, xmin
-
+	 
 	 If (array_size .LE. 4) then
 	    area_hull = array_size
 	 else
 	    area_hull = 0
 	    Do y=ymin, ymax
 	       xleft = 9999
-	       xright = 0
-	       !x = -1
+	       xright = 0	       
 
 	       x=0
 	       k=0
@@ -708,20 +586,17 @@ MODULE m_mode_singleAttrib
 
 	   	  xmayor = set(i)%x
 		  xmenor = set(j)%x
-		  !print*, 'i,j, xmayor, xmenor', i,j, xmayor, xmenor
 
 		  if (xmayor .LT. xmenor) then
 		     temp = xmayor
 		     xmayor = xmenor
 		     xmenor = temp
 		  endif
-		  !print*, 'xmayor, xmenor', xmayor, xmenor
 
 		  if (set(j)%y .NE. set(i)%y) then
-		     x = (xmin*y - y*xmax)*(set(j)%x - set(i)%x) - (xmin - xmax)*(set(j)%x*set(i)%y - set(j)%y*set(i)%x)
-		     !print*, 'x', x
+		     x = (xmin*y - y*xmax)*(set(j)%x - set(i)%x) - (xmin - xmax)*(set(j)%x*set(i)%y - set(j)%y*set(i)%x)		     
                      x = x /((xmin - xmax)*(set(j)%y - set(i)%y))
-		     !print*, 'x1', x
+		     
 		  else
 		     if ( (k .EQ. 1) .OR. (k .EQ. 2) ) then
 		        k = k + 1
@@ -730,7 +605,6 @@ MODULE m_mode_singleAttrib
 		        k = k + 1
 		     endif
 		  endif
-		  !print*, 'k, area_hull', k, area_hull
 
 		  if ( (x .LT. xmayor) .AND. (x .GE. xmenor)) then
 		     if (x .LT. xleft) then
