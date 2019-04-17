f1a325b12beced70200ebf14f2e0e8d3eb1a78ff
carlos.frederico <carlos.frederico@f4d869c9-3b79-40b0-af35-19216b5cd462>
2019-04-17 12:47:54 +0000

    Atualizações no script "scanplot_scorecard.py":
    * Alterada a barra de cores com as anotações "pior" e "melhor" nos extremos;
    * Corrigido o nome do cálculo "mudança fracional" para "ganho";
    * Adicionado o cálculo da "mudança fracional";
    * Corrigido o cálculo do "ganho";
    * Alterações menores nos títulos, tamanho das figuras e nomes dos arquivos salvos.

--- branches/wsantos.t4084/SCANPLOT/scanplot_scorecard.py
+++ branches/wsantos.t4084/SCANPLOT/scanplot_scorecard.py
@@ -2,25 +2,30 @@
 # -*- coding: utf-8 -*-
 
 """
-Objetivo: Este script calcula o "Fractional Change*" a partir das estatísticas 
-          do SCANTEC e plota os resultados na forma de um scorecard.
+Objetivo: Este script calcula o "Ganho Percentual" e a "Mudança Fracional*" a 
+          partir das estatísticas do SCANTEC e plota os resultados na forma de 
+          um scorecard.
 
 Uso: python3 scanplot_scorecard.py
 
 Observações: Alterar o valor das variáveis indicadas abaixo.
 
-*Banos et al., 2018: Impacto da Assimilação de Perfis de Refratividade do Satélite Metop-B nas
-Previsões de Tempo do CPTEC/INPE Durante os Meses de Janeiro e Agosto de 2014
+*Banos et al., 2018: Impacto da Assimilação de Perfis de Refratividade do 
+                     Satélite Metop-B nas Previsões de Tempo do CPTEC/INPE 
+                     Durante os Meses de Janeiro e Agosto de 2014
 
 Versões dos pacotes:
 """
 
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 Stats = ["VIES", "RMSE", "ACOR"]
 Regs = ["hn", "hs", "tr"]
+#Stats = ["RMSE"]
+#Regs = ["tr"]
 
 bexp1 = "SMG_V2.0.0"
 bexp2 = "SMG_V2.1.0"
@@ -29,6 +34,9 @@ datai = "2015050200"
 dataf = "2015053100"
 tipo = "mensal"
 hsin = "00Z"
+
+# Opções: "ganho" ou "fc"
+tstat = "fc"
 
 base_path = "/home/carlos/Documents/INPE2019/GDAD/SMG/teste_scamtec_v100/repo_carlos/wsantos.t4084/SCANPLOT/dadosscantec/aval_SMG/"
 
@@ -94,20 +102,53 @@ for stat in Stats:
                                           "PREV-000"])
     
         # Cálculo do Fractional Change
-        if stat == "ACOR":
-            score_table = ((p_table1[1:-1].T - p_table2[1:-1].T) / (1. - p_table2[1:-1].T)) * 100
-        elif stat == "RMSE" or stat == "VIES":
-            score_table = ((p_table1[1:-1].T - p_table2[1:-1].T) / (0. - p_table2[1:-1].T)) * 100
+        if tstat == "ganho":
+            if stat == "ACOR":
+                score_table = ((p_table2[1:-1].T - p_table1[1:-1].T) / (1.0 - p_table1[1:-1].T)) * 100
+            elif stat == "RMSE" or stat == "VIES":
+                score_table = ((p_table2[1:-1].T - p_table1[1:-1].T) / (0.0 - p_table1[1:-1].T)) * 100
+        elif tstat == "fc":
+                score_table = (1.0 - (p_table2[1:-1].T / p_table1[1:-1].T))
             
         # Figura
-        sns.set(style="darkgrid", font_scale=0.5)
-            
-        fig, ax = plt.subplots()    
-            
-        plt.plot(figsize=(40,40))
-        plt.title("Fractional Change " + str(stat) + " " + str(reg.upper()) + " (%)\nSMG Experimentos V2.0.0 e V2.1.0", fontsize=10)
-        plot = sns.heatmap(score_table, annot=True, fmt="1.0f", cmap="RdYlGn", 
-                           cbar_kws={"label": stat}, vmin=-100, vmax=100, center=0,
-                           linewidths=.25)
+        fig = plt.subplots(figsize=(5,4))
+
+        sns.set(style="whitegrid", font_scale=0.45)
+        sns.set_context(rc={"xtick.major.size": 1.5,  "ytick.major.size": 1.5,
+                            "xtick.major.pad": 0.05,  "ytick.major.pad": 0.05,
+                            "xtick.major.width": 0.5, "ytick.major.width": 0.5,
+                            "xtick.minor.size": 1.5,  "ytick.minor.size": 1.5,
+                            "xtick.minor.pad": 0.05,  "ytick.minor.pad": 0.05,
+                            "xtick.minor.width": 0.5, "ytick.minor.width": 0.5})
+        
+        if tstat == "ganho":
+            plt.title("Ganho " + str(stat) + " " + str(reg.upper()) + " " + str(hsin) + " (%)\n" + str(bexp1) + " X " + str(bexp2), fontsize=10)
+        elif tstat == "fc":
+            plt.title("Mudança Fracional " + str(stat) + " " + str(reg.upper()) + " " + str(hsin) + "\n" + str(bexp1) + " X " + str(bexp2), fontsize=10)
+
+        if tstat == "ganho":
+            plot = sns.heatmap(score_table, annot=True, fmt="1.0f", cmap="RdYlGn", 
+                               vmin=-100, vmax=100, center=0, linewidths=.25,
+                               cbar_kws={"shrink": 1.0, 
+                                         "ticks": np.arange(-100,110,10),
+                                         "pad": 0.01,
+                                         "orientation": "vertical"})
+    
+            cbar = plot.collections[0].colorbar
+            cbar.set_ticks([-100, -50, 0, 50, 100])
+            cbar.set_ticklabels(["pior", "-50%", "0", "50%", "melhor"])
+        elif tstat == "fc":
+            plot = sns.heatmap(score_table, annot=True, fmt="1.0f", cmap="RdYlGn", 
+                               vmin=-1, vmax=1, center=0, linewidths=.25, xticklabels=False,
+                               cbar_kws={"shrink": 1.0, 
+                                         "ticks": np.arange(-1,2,1),
+                                         "pad": 0.01,
+                                         "orientation": "vertical"})
+    
+            cbar = plot.collections[0].colorbar
+            cbar.set_ticks([-1, -0.5, 0, 0.5, 1])
+            cbar.set_ticklabels(["pior", "-0.5", "0", "0.5", "melhor"])
+        
+        plt.xlabel("Previsões")
         plt.tight_layout()
-        plt.savefig("scorecard_exps" + "_" + str(stat.lower()) + "_" + str(reg) + "_" + str(datai) + "-" + str(dataf) + ".png", bbox_inches="tight", dpi=200)
+        plt.savefig("scorecard_" + str(tstat) + "_exps" + "_" + str(stat.lower()) + "_" + str(reg) + "_" + str(datai) + "-" + str(dataf) + "_" + str(hsin) + ".png", bbox_inches="tight", dpi=200)
