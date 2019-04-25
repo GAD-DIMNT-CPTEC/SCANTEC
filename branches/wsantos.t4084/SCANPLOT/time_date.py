cbd0bde31718f47f47fa02b34567b200dbe27c35
wanderson.santos <wanderson.santos@f4d869c9-3b79-40b0-af35-19216b5cd462>
2019-04-25 21:20:28 +0000

    Foi criado uma classe para padronizar a entrada dos parametros diaInicial,diaFinal,mes e ano para os script scanplot_diario_jn.py e scanplot_mensal_jn.py.

--- branches/wsantos.t4084/SCANPLOT/time_date.py
+++ branches/wsantos.t4084/SCANPLOT/time_date.py
@@ -1,25 +1,53 @@
 from datetime import timedelta, date
 
+# Ideia da função retirada desse site
+#Como utiliza-lo:
+# retirado desse site:
+# https://www.w3resource.com/python-exercises/date-time-exercise/python-date-time-exercise-50.php
+
+# Exemplo de como utilizar a função
+#start_dt = date(2015, 5, 1)
+##end_dt = date(2015, 5, 5)
+#for dt in daterange(start_dt, end_dt):
+######print(dt.strftime("%Y-%m-%d"))
+
 def daterange(date1, date2):
     for n in range(int ((date2 - date1).days)+1):
         yield date1 + timedelta(n)
 
-# Como utiliza-lo:
-# retirado desse site:
-# https://www.w3resource.com/python-exercises/date-time-exercise/python-date-time-exercise-50.php
 
-#start_dt = date(2015, 5, 1)
-#end_dt = date(2015, 5, 5)
-#for dt in daterange(start_dt, end_dt):
-#    #print(dt.strftime("%Y-%m-%d"))
-#    #print(dt.strftime("%Y%m%d"))
-#    anomesdia = dt.strftime("%Y%m%d")
-#    dia = dt.strftime("%d")
-#    mes = dt.strftime("%m")
-#    ano = dt.strftime("%Y")
-#    ####print("dia: "+str(dia))
-#    ####print("mes: "+str(mes))
-#    ####print("ano: "+str(ano))
-#    
-#    #print(dt.strftime("%Y-%m-%d"))
-#    print("ano-mes-dia "+str(anomesdia))
+# Classe criada para passar os parametros de data
+
+# Exemplo de como utilizar a classe
+#p = definirData(1,4,5,2015)
+#print(p.__str__)
+
+
+class definirData:
+        def __init__(self,diaInicial,diaFinal,mes,ano):
+                self.diaI = diaInicial  
+                self.diaF = diaFinal
+                self.mes  = mes
+                self.ano  = ano
+        
+        def getDiaInicial(self):
+                return self.diaI
+        
+        def getDiaFinal(self):
+                return self.diaF
+        
+        def getmes(self):
+                return self.mes
+        
+        def getano(self):
+                return self.ano
+        
+        def getStart(self):
+                return date(self.ano,self.mes,self.diaI)
+        
+        def getEnd(self):
+                return date(self.ano,self.mes,self.diaF)
+        
+
+
+
